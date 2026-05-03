ed34f1bea6b218ec8db753b602012f4abff83455
dannsk94 <dannsk@inbox.ru>
2026-05-03 16:52:39 +0700

    Delete sqlite-final directory

--- sqlite-final/task8/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <fmt/core.h>
-using namespace fmt;
-//fmt::print не встроена в с++ и ее мы загрузим через conan.
-int main() {
-    print("Результат работы задания {}\n", 8);
-    return 0;
-}
